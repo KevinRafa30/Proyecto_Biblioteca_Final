a7c0e8232ce3b8bd69f94a59fb746287cc117a40
Ambar Santos <santosambar738@gmail.com>
2026-02-20 17:18:04 -0400

    Agregando namespaces a program.cs

--- BibliotecaUNAPEC_Web/Program.cs
+++ BibliotecaUNAPEC_Web/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Biblioteca_Unapec.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
