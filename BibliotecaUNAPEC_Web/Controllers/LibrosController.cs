3367e081d36d66b4ecbe4dfd18868a55f8e7e45f
Ambar Santos <santosambar738@gmail.com>
2026-03-04 16:19:10 -0400

    Seguridad en controlador para libro
    - Authrize para modificacion autorizada
    - ValidateAntiForgeryToken para ataques de suplantacion de identidad
    - Bind para evitar overposting

--- BibliotecaUNAPEC_Web/Controllers/LibrosController.cs
+++ BibliotecaUNAPEC_Web/Controllers/LibrosController.cs
@@ -23,5 +23,38 @@ namespace BibliotecaUNAPEC_Web.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Para ataques de suplantación de identidad (CSRF)
+        public async Task<IActionResult> Create([Bind("IdLibro,Titulo,Isbn,AnioPublicacion,IdAutor")] Libro libro) //El Bind es para especificar qué propiedades del modelo se deben enlazar desde el formulario,
+                                                                                                                   //lo que ayuda a prevenir ataques de sobrepublicación (over-posting) al limitar
+                                                                                                                   //los campos que pueden ser modificados por el usuario.
+                                                                                                         
+        {
+            
+            // Revisando que el ISBN y Título no estén vacíos antes de guardar.
+            if (ModelState.IsValid)
+            {
+                _context.Add(libro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index)); // Hay que hacer index
+            }
+
+            // En caso de un fallo, volver a la vista para mostrar los errores
+            return View(libro);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Lista de libros con sus autores incluidos
+            var libros = await _context.Libros
+                .Include(l => l.IdAutorNavigation)
+                .ToListAsync();
+
+            return View(libros);
+        }
+
+ 
+     
     }
 }
