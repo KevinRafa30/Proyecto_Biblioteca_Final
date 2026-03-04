ddc2e92f706214221d4138dfea5b27221c3d0a5a
Ambar Santos <santosambar738@gmail.com>
2026-03-04 16:07:26 -0400

    inyeccion en la base de datos y formulario para crear nuevo libro

--- BibliotecaUNAPEC_Web/Controllers/LibrosController.cs
+++ BibliotecaUNAPEC_Web/Controllers/LibrosController.cs
@@ -8,7 +8,18 @@ namespace BibliotecaUNAPEC_Web.Controllers
     [Authorize]
     public class LibrosController : Controller
     {
-        public IActionResult Index()
+
+        private readonly BibliotecaUnapecContext _context;
+
+        //inyeccion de la base de datos
+        public LibrosController(BibliotecaUnapecContext context)
+        {
+            _context = context;
+        }
+
+        //formulario para nuevo libro
+        //get /Libros/Create
+        public IActionResult Create()
         {
             return View();
         }
