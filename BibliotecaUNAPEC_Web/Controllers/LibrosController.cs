d4ad44bc3571dafb5d0391e1e76b5e37b7f56a66
Ambar Santos <santosambar738@gmail.com>
2026-03-04 16:03:41 -0400

    crear controlador para libros y agregar usings

---
+++ BibliotecaUNAPEC_Web/Controllers/LibrosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BibliotecaUNAPEC_Web.Models; 
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaUNAPEC_Web.Controllers
+{
+    public class LibrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
