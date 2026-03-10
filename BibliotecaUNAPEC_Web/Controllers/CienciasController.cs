a1614b5275d098a83205dade8c354180d8f63700
Ambar Santos <santosambar738@gmail.com>
2026-03-10 10:57:55 -0400

    Creacion de controllers con views faltantes

---
+++ BibliotecaUNAPEC_Web/Controllers/CienciasController.cs
@@ -0,0 +1,158 @@
+﻿using BibliotecaUNAPEC_Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaUNAPEC_Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CienciasController : Controller
+    {
+        private readonly BibliotecaUnapecContext _context;
+
+        public CienciasController(BibliotecaUnapecContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ciencias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ciencias.ToListAsync());
+        }
+
+        // GET: Ciencias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ciencia = await _context.Ciencias
+                .FirstOrDefaultAsync(m => m.IdCiencia == id);
+            if (ciencia == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciencia);
+        }
+
+        // GET: Ciencias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ciencias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCiencia,Descripcion,Estado")] Ciencia ciencia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ciencia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciencia);
+        }
+
+        // GET: Ciencias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ciencia = await _context.Ciencias.FindAsync(id);
+            if (ciencia == null)
+            {
+                return NotFound();
+            }
+            return View(ciencia);
+        }
+
+        // POST: Ciencias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCiencia,Descripcion,Estado")] Ciencia ciencia)
+        {
+            if (id != ciencia.IdCiencia)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ciencia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CienciaExists(ciencia.IdCiencia))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciencia);
+        }
+
+        // GET: Ciencias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ciencia = await _context.Ciencias
+                .FirstOrDefaultAsync(m => m.IdCiencia == id);
+            if (ciencia == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciencia);
+        }
+
+        // POST: Ciencias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ciencia = await _context.Ciencias.FindAsync(id);
+            if (ciencia != null)
+            {
+                _context.Ciencias.Remove(ciencia);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CienciaExists(int id)
+        {
+            return _context.Ciencias.Any(e => e.IdCiencia == id);
+        }
+    }
+}
---
+++ BibliotecaUNAPEC_Web/Controllers/TiposBibliografiumsController.cs
@@ -0,0 +1,158 @@
+﻿using BibliotecaUNAPEC_Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaUNAPEC_Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class TiposBibliografiumsController : Controller
+    {
+        private readonly BibliotecaUnapecContext _context;
+
+        public TiposBibliografiumsController(BibliotecaUnapecContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TiposBibliografiums
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TiposBibliografia.ToListAsync());
+        }
+
+        // GET: TiposBibliografiums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tiposBibliografium = await _context.TiposBibliografia
+                .FirstOrDefaultAsync(m => m.IdTipoBibliografia == id);
+            if (tiposBibliografium == null)
+            {
+                return NotFound();
+            }
+
+            return View(tiposBibliografium);
+        }
+
+        // GET: TiposBibliografiums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TiposBibliografiums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTipoBibliografia,Descripcion,Estado")] TiposBibliografium tiposBibliografium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tiposBibliografium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tiposBibliografium);
+        }
+
+        // GET: TiposBibliografiums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tiposBibliografium = await _context.TiposBibliografia.FindAsync(id);
+            if (tiposBibliografium == null)
+            {
+                return NotFound();
+            }
+            return View(tiposBibliografium);
+        }
+
+        // POST: TiposBibliografiums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTipoBibliografia,Descripcion,Estado")] TiposBibliografium tiposBibliografium)
+        {
+            if (id != tiposBibliografium.IdTipoBibliografia)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tiposBibliografium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TiposBibliografiumExists(tiposBibliografium.IdTipoBibliografia))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tiposBibliografium);
+        }
+
+        // GET: TiposBibliografiums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tiposBibliografium = await _context.TiposBibliografia
+                .FirstOrDefaultAsync(m => m.IdTipoBibliografia == id);
+            if (tiposBibliografium == null)
+            {
+                return NotFound();
+            }
+
+            return View(tiposBibliografium);
+        }
+
+        // POST: TiposBibliografiums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tiposBibliografium = await _context.TiposBibliografia.FindAsync(id);
+            if (tiposBibliografium != null)
+            {
+                _context.TiposBibliografia.Remove(tiposBibliografium);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TiposBibliografiumExists(int id)
+        {
+            return _context.TiposBibliografia.Any(e => e.IdTipoBibliografia == id);
+        }
+    }
+}
